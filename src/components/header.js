9d4b73f0c56389b39a949d036e320b21b2bbcd06
Carter Rink <carter.m.rink@gmail.com>
2020-01-11 22:02:24 -0500

    added highlight animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,5 @@
 import React from "react"
-import styled, { keyframes } from 'styled-components';
-
-const changeColor = keyframes`
-  from {
-    color: #000000;
-    text-shadow: 0;
-  }
-
-  to {
-    color: #f2f2f2;
-        text-shadow: 0.222rem 0.222rem 0.444rem rgba(211, 211, 211, 0.75), calc(0.222rem * -1) calc(0.222rem * -1) 0.444rem rgba(255, 255, 255, 0.75);
-  }
-  `;
+import styled from "styled-components"
 
 const Header =styled.h1`
     display: flex;
@@ -21,9 +9,14 @@ const Header =styled.h1`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     margin: 0;
     width: 100%;
-    animation: ${changeColor};
-    animation-duration: 5s;
-    animation-fill-mode: forwards;
+    transition: 0.2s;
+    &:active {
+        color: #f2f2f2;
+        text-shadow: 0.222rem 0.222rem 0.444rem rgba(211, 211, 211, 0.75), calc(0.222rem * -1) calc(0.222rem * -1) 0.444rem rgba(255, 255, 255, 0.75);
+    }
+    ::selection {
+        color: #000000;
+    }
 `
 
 export default props => <Header>{props.headerText}</Header>
