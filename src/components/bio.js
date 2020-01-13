af04fa7453f5928b0be65b35992416db595dd9a9
Carter Rink <carter.m.rink@gmail.com>
2020-01-12 19:57:25 -0500

    updated bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -18,5 +18,5 @@ const Bio = styled.div`
 `
 
 export default () => (
-    <Bio>Hi 👋, I'm a font-end developer and designer located in Boston, MA. I'm currently a developer at WGBH. They make some great shows like Frontline and NOVA.  I'm also available to freelance. I specialize in web and email development. Take a look around and drop me a line below.</Bio>
+    <Bio>Hi 👋, I'm a font-end developer and designer located in Boston. I'm currently a developer at WGBH. They make some great shows like Frontline and NOVA.  I'm also available to freelance. I specialize in web and email development. Take a look around and drop me a line below.</Bio>
 )
