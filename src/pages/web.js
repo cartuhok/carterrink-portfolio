125542f08aae90edcc6bd75edb8f92b87c8abf2c
Carter Rink <carter.m.rink@gmail.com>
2020-02-09 16:28:37 -0500

    added codepen

--- src/pages/web.js
+++ src/pages/web.js
@@ -22,6 +22,6 @@ export default () => (
   <div>
     <Back />
     <WebGrid />
-    <WebDesc><span style={{ color: "#e35656" }}>*</span>this site is developed using&nbsp;<a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer" style={{color: `#000000`}}>Gatsby</a>&nbsp;and&nbsp;<a href="https://www.styled-components.com/" target="_blank" rel="noopener noreferrer" style={{color: `#000000`}}>Styled Components</a>.</WebDesc>
+    <WebDesc><span style={{ color: "#e35656" }}>*</span>this site is developed using&nbsp;<a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer" style={{color: `#000000`}}>Gatsby</a>&nbsp;and&nbsp;<a href="https://www.styled-components.com/" target="_blank" rel="noopener noreferrer" style={{color: `#000000`}}>Styled Components</a>. You can see other examples of my work on my&nbsp;<a href="https://www.styled-components.com/" target="_blank" rel="noopener noreferrer" style={{color: `#000000`}}>Codepen</a>.</WebDesc>
   </div>
 )
