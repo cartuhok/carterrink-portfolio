83a75ad5b378b0dec57b5ae908878e645b4f9abd
Carter Rink <carter.m.rink@gmail.com>
2020-02-09 01:02:19 -0500

    added scroll effect to name

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,35 @@
 import React from "react"
-import styled from "styled-components"
 
-const Header =styled.h1`
-    display: flex;
-    font-size: 14vw;
-    text-align: center;
-    justify-content: center;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    margin: 0;
-    width: 100%;
-    transition: 0.2s;
-    &:active {
-        color: #f2f2f2;
-        text-shadow: 0.222rem 0.222rem 0.444rem rgba(211, 211, 211, 0.75), calc(0.222rem * -1) calc(0.222rem * -1) 0.444rem rgba(255, 255, 255, 0.75);
-    }
-    ::selection {
-        color: #000000;
-    }
-`
 
-export default props => <Header>{props.headerText}</Header>
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            hasScrolled: false
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = (event) => {
+        const scrollTop = window.pageYOffset
+
+        if (scrollTop > 10) {
+            this.setState({ hasScrolled: true })
+        } else {
+            this.setState({ hasScrolled: false })
+        }
+    }
+
+    render() {
+        return (
+            <h1 className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>CARTER RINK</h1>
+        )
+    }
+}
+
+
+export default Header
